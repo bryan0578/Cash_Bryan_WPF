dba696d62b392d58591151839f16814684abc42c
bryan0578 <bryan.cash78@gmail.com>
2014-01-13 09:24:12 -0500

    added alert the area of the rectangle

--- class 3 WPF/Prompts and Alerts/AssociatingJS/js/script.js
+++ class 3 WPF/Prompts and Alerts/AssociatingJS/js/script.js
@@ -4,3 +4,5 @@ var width = prompt("We are calculating the area of a rectangle. \nPlease enter t
 var height = prompt("Please enter the height:");
 var area = width * height;
 console.log(area);
+
+alert("The area of your rectangle is " + area)
