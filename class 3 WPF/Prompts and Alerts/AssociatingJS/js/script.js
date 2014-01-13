af869ad361148f74c049e056e88b1fd60091b1f2
bryan0578 <bryan.cash78@gmail.com>
2014-01-13 09:14:58 -0500

    changed line three to include new line at the end of the first string

--- class 3 WPF/Prompts and Alerts/AssociatingJS/js/script.js
+++ class 3 WPF/Prompts and Alerts/AssociatingJS/js/script.js
@@ -1,4 +1,4 @@
 //prompt and alert
 
-var width = prompt("We are calculating the area of a rectangle. Please enter the width:");
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width:");
 var height = prompt("Please enter the height:");
