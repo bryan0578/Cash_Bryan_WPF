26311f5636bf117d781cd340606d5f652a6c2851
bryan0578 <bryan.cash78@gmail.com>
2014-01-14 21:16:59 -0500

    Started expressions worksheet coded problems dog years and slice of pie part 1

--- class 3 WPF/Expressions/age example/AssociatingJS/js/script.js
+++ class 3 WPF/Expressions/age example/AssociatingJS/js/script.js
@@ -1,5 +1,47 @@
 //Expressions
 
-var yearBorn = 1978;
-var age = 2012 - yearBorn;
-console.log(age);
+//Name: Bryan Cash
+//Assignment: Expressions Worksheet
+//Date: 1/14/2014
+
+
+//Dogs age 7 times faster than humans so a dog that is 1 year old in human years is 7 years old in “dog years.”
+//Calculate how old Sparky the pit bull is in dog years based on his actual age.
+
+var sparkysAge = 7;
+
+const DOGYEARS = 7;
+
+//multiply SparkysAge and dogAge and assign variable dogYears
+
+var dogAge = sparkysAge * DOGYEARS
+
+//print out the sparkys age in dog years
+
+console.log("Sparky is " + sparkysAge + " human years old which is " + dogAge + " in dog years!");
+
+
+
+//A bunch of students are having a party and somebody ordered pizza. Create an expression that calculates how much pizza
+//each party-goer will get at the party.  (Assume all pizzas have the same number of slices and that the person dividing
+//the pizza is really precise, so this can be a decimal, like 3.52 slices, etc.)
+
+var students = 20;
+var pizza = 10;
+const SLICES = 8;
+
+//Multiply pizza and slices and then divide by students
+
+var slicePerStudent = pizza * SLICES / students;
+
+//Print variable slicePerStudent
+
+console.log("Each person ate " + slicePerStudent + " slices of pizza at the party.");
+
+
+
+
+
+
+
+
