83cd29dc6552862b23e921e5c1b99f8954ce421b
bryan0578 <bryan.cash78@gmail.com>
2014-01-16 15:11:43 -0500

    set up all variables with prompts

---
+++ class 3 WPF/Expression_Personal/AssociatingJS/js/script.js
@@ -0,0 +1,27 @@
+//Name:Bryan Cash
+//Date:1/16/2014
+//Assignment: Expressions
+
+
+//Expression_Personal:This program source code is meant to take in data from user prompts to calculate how much money
+//would have left over each month to spend on entertainment or savings
+
+
+
+
+var monthlyPay = Number(prompt("Please enter the exact amount of your monthly income, if there is no income please enter 0"));
+var rent = Number(prompt("Please enter the amount you pay for rent each month. If you do not pay rent please enter 0"));
+var utilities = Number(prompt("Please enter the COMBINED total of all your utility bills. If you do not pay any utilities," +
+    "please enter 0"));
+var groceries = Number(prompt("Please enter the amount of your monthly average grocery spending. If you do not pay for" +
+    "groceries, please enter 0"));
+var gas = Number(prompt("Please enter the amount of your monthly average spent on gasoline. If you ride the bus you may enter" +
+    "your monthly average in bus fair. Otherwise please enter 0"));
+var car = Number(prompt("Please enter your monthly car payment, if you do not pay a car payment, please enter 0"));
+var mortgage = Number(prompt("Please enter your monthly mortgage payment, if you do not pay a mortgage please enter 0"));
+var personalLoans = Number(prompt("Please enter your monthly amount is personal or student loans. If you have more than one please" +
+    "the COMBINED total of all loans, otherwise please enter 0"));
+var cellPhone = Number(prompt("Please enter your monthly cellphone payment. If you do not have a cellphone please enter 0"));
+var neccessities = Number(prompt("Please enter the average amount that you spend on neccessities, such as cleaning products, " +
+    "haircut, medicine, cloths, ect. "));
+
