a7b19bc737007454695e11012689ce96d5844489
bryan0578 <bryan.cash78@gmail.com>
2014-01-16 20:34:17 -0500

    Start of Expression_Industry added variables for greeting alert

---
+++ class 3 WPF/Expression_Industry/AssociatingJS/js/script.js
@@ -0,0 +1,14 @@
+//Name: Bryan Cash
+//Date:1/16/2014
+//Assignment: Expressions
+
+
+//Expression_Industry
+
+var firstName = prompt("Please enter your first name");
+var lastName = prompt("Please enter your last name");
+var storeNumber = prompt("Please enter store number");
+var date = prompt("Please enter the date");
+
+alert("Wlecome " + firstName +" " + lastName);
+
