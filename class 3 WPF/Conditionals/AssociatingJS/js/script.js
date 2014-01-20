8ccefa7dbfd4aa7943eddd36b53e3c20a01f3c01
bryan0578 <bryan.cash78@gmail.com>
2014-01-20 15:25:13 -0500

    started conditionals screencast, first conditional screencast oldEnough variable set to true

---
+++ class 3 WPF/Conditionals/AssociatingJS/js/script.js
@@ -0,0 +1,10 @@
+//Conditionals Screencast Follow-along
+
+var oldEnough = true;
+
+//if the chile is old enough, then print to the console "You can ride!"
+
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster");
+}
