c4423ed705ff53a0baf39a2bed26d04fb064b259
bryan0578 <bryan.cash78@gmail.com>
2014-01-20 15:41:56 -0500

    Added minHeight variable, changed if condition to if(kidHeight > minHeight)

--- class 3 WPF/Conditionals/AssociatingJS/js/script.js
+++ class 3 WPF/Conditionals/AssociatingJS/js/script.js
@@ -12,10 +12,11 @@ if(oldEnough){
 //Conditionals Screencast Relational Expressions
 
 var kidHeight = 50;
+var minHeight = 48;
 
 //if the kid is over 48 inches in height
 
-if(kidHeight > 48){
+if(kidHeight > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster");
 }
