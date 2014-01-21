f3281aff520297627be22d445aa5f4f873fcac2a
bryan0578 <bryan.cash78@gmail.com>
2014-01-20 22:21:13 -0500

    Started conditionals if/else statement using kidHeight and minHeight added condition kidHeight < minHeight

--- class 3 WPF/Conditionals/AssociatingJS/js/script.js
+++ class 3 WPF/Conditionals/AssociatingJS/js/script.js
@@ -36,3 +36,18 @@ if(kidHeight + sneakerLift > minHeight){
 
 //Conditionals Screen cast If and Else
 
+var kidHeight = 47;
+var minHeight = 48;
+
+//if the kid is over 48 inches in height
+
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster");
+}
+if(kidHeight < minHeight){
+    console.log("Sorry kid, you've got some growing to do first!");
+}
+
+
+
