f040b552e7defcca1c833b96e155365031048478
bryan0578 <bryan.cash78@gmail.com>
2014-01-21 00:20:38 -0500

    started conditionals ternary statements show if else statement in comments then ternary statement

--- class 3 WPF/Conditionals/AssociatingJS/js/script.js
+++ class 3 WPF/Conditionals/AssociatingJS/js/script.js
@@ -62,6 +62,29 @@ if(iPhonePrice < budget || wonLottery === true){
     console.log("No phone for you!");
 }
 
+//Conditionals Screen cast Ternary Operators
 
+var gpa =48;
 
+//if gpa is over the min 2.0 score, the student can graduate
+/*if(gpa > 2.0){
+    console.log("You can graduate");
+}else{
+    console.log("Gpa is too low");
+}*/
 
+(gpa > 2.0) ? console.log("You can graduate") : console.log("Gpa is too low");
+
+var age = 11;
+var book;
+
+//if the child is < 10 they will get Green Eggs and Ham, otherwise they will get The Time Machine
+/*if(age < 10){
+    book = "Green Eggs and Ham";
+}else{
+    book = "The Time Machine";
+}
+console.log(book);*/
+
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book)
