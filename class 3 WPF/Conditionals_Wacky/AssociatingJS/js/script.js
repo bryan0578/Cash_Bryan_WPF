b9fae33c6174c83c98bbedcf3686b8a4f638baa5
bryan0578 <bryan.cash78@gmail.com>
2014-01-23 02:16:24 -0500

    Started conditionals_wacky set up opening alerts, variables and prompts with comments

--- class 3 WPF/Conditionals_Wacky/AssociatingJS/js/script.js
+++ class 3 WPF/Conditionals_Wacky/AssociatingJS/js/script.js
@@ -1,1 +1,19 @@
-alert("This is in the js file");
+//Name: Bryan Cash
+//Date: 1/22/2014
+//Assignment: Conditionals_Wacky
+
+//Are you an apprentice or a master? Choose your side and take over or protect the galaxy.
+
+//This program takes your age and determines if you are an apprentice or a master and lets you choose a side dark or
+//light and them welcomes you to your destiny
+
+//Alert user about the program
+
+alert("Are you an apprentice or a master?");
+alert("Choose your destiny!");
+
+//prompt user to enter age to determine whether or not they are an apprentice or a master
+
+var age = prompt("Please Enter your age");
+var side = prompt("Please Enter Light for Light-side or Dark for Dark-side");
+
