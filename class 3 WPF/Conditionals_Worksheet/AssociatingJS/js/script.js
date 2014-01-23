3e68d5a8dc5565f62b2bfa6250fa651d0b15485c
bryan0578 <bryan.cash78@gmail.com>
2014-01-22 22:13:21 -0500

    Started conditionals_personal and set up variables milk, eggs and answer for bread

---
+++ class 3 WPF/Conditionals_Worksheet/AssociatingJS/js/script.js
@@ -0,0 +1,17 @@
+//Name: Bryan
+//Date: 01/22/2014
+//Assignment Conditionals Worksheet
+
+//Convert a temperature to either degrees Celsius or degrees Fahrenheit depending on what the user has entered.
+// If the user puts a “C” for the unit, the calculator should convert to Celsius. If the user puts “F” for the unit,
+// the calculator should convert to Fahrenheit.
+
+//Given:Degrees (in F or C)
+//Unit (a string holding an “F” or a “C”)
+//Result: “The temperature is X degrees Celsius.” Or “The temperature is X degrees Fahrenheit.”
+
+
+
+var degCent = prompt("Please enter the degree in celsius" + (degFarh - 32) * 5 / 9)
+var degFarh = prompt("Please enter the degree in fahrenheit" + (degCent * 9) / 5 +32)
+var unit = prompt("Please enter C for celsius or F for fahrenheit")
