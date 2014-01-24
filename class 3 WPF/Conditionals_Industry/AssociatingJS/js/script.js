529b0dbb4524f7afd91f0ed43a6a5840f1f4f0e6
bryan0578 <bryan.cash78@gmail.com>
2014-01-23 19:34:59 -0500

    started conditionals_industry created comment to introduce program and comments on the opening prompts wrote code and tested program

--- class 3 WPF/Conditionals_Industry/AssociatingJS/js/script.js
+++ class 3 WPF/Conditionals_Industry/AssociatingJS/js/script.js
@@ -1,1 +1,33 @@
-alert("This is in the js file");
+//Name: Bryan Cash
+//Date: 1/22/2014
+//Assignment: Conditionals_Industry
+
+//This program will determine what settings to use to display a web page.
+
+//Prompt user to enter screen dimensions for their desktop or to enter "mobile" if they are using a mobile device.
+//Prompt user to enter the screen dimensions for their mobile device or enter "desktop" if they are using a desktop
+//computer.
+
+
+var desktop = prompt("If you are using a desktop please enter 1080x1240. If you are using a mobile device, enter mobile");
+var mobile = prompt("If you are using a mobile phone please enter one of the followig: for an Lg or Samsung, enter 400x240," +
+    " for an Apple iPhone, enter 480x320, for a Nokia, enter 640x360, or for a Sony enter 800x400 if you are using a" +
+    " desktop enter desktop.");
+
+if(desktop == "1080x1240" && mobile == "desktop"){
+    console.log("Your web page will be configured for a desktop computer.");
+
+}else if(desktop == "mobile" && mobile == "400x240"){
+    console.log("Your web page will be configured for an LG or Samsung mobile phone.");
+
+}else if(desktop == "mobile" && mobile == "480x320"){
+    console.log("Your web page will be configured for an Apple iPhone.");
+
+}else if(desktop == "mobile" && mobile == "640x360"){
+    console.log("Your web page will be configured for a Nokia mobile phone.");
+
+}else if(desktop == "mobile" && mobile == "800x400"){
+    console.log("Your desktop will be configured for a Sony mobile phone")
+}else{
+    console.log("Your device is not compatible with our web page.")
+}
