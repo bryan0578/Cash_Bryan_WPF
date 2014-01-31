c43c5432cf5e7f5de9538b65454d15114e532d94
bryan0578 <bryan.cash78@gmail.com>
2014-01-30 23:25:02 -0500

    added comments to explain variables

--- class 3 WPF/Functions_Wacky/js/script.js
+++ class 3 WPF/Functions_Wacky/js/script.js
@@ -8,9 +8,20 @@
 
     //This program will calculate the chances that the republic will win against the separatists
 
+    //prompt user to input the number of jedi that will be fighting
+
 var jedi = Number(prompt("Please enter the amount of Jedi in your Jedi Counsel"));
+
+//prompt user to input the number of sith they will fight against
+
 var sith = Number(prompt("Please enter the amount of sith you will fighting"));
+
+//prompt user to input the amount of clones that will be fighting
+
 var clones = Number(prompt("Please enter the amount of Republic troops"));
+
+//prompt user to input the number of droids they will be fighting against
+
 var droids = Number(prompt("Please enter the amount of separatist droids"));
 
 function calc(jedi, clones, sith, droids){
