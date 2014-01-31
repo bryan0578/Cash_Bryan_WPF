c68c0861e59cb46bacb94989db7dab743a072c47
bryan0578 <bryan.cash78@gmail.com>
2014-01-30 19:50:15 -0500

    added comments to first two prompts and first condition

--- class 3 WPF/Functions_Industry/js/script.js
+++ class 3 WPF/Functions_Industry/js/script.js
@@ -5,14 +5,20 @@
 //Bonus Calculator - This portion of code will calculate how much money a person can earn for exceeding their production
 //goals
 
+//prompt user to input their production goal number and the number of products their packaged in the day
+
 var production = Number(prompt("What was your teams production goal? Please enter the number of products expected to be" +
     " packaged today according to your team's production goal"));
 var products = Number(prompt("How many products did you package today?"));
 
+//created ternary condition if production number is less than the number of products alert the user they have exceeded
+//their production goals if condition is false alert the user they will not receive a bonus because they have not exceeded
+//their production goals
 
     bonus1 = (production < products) ? "You have exceeded your productions goals for today" : "You have not exceeded" +
         " your production goals for today. You will not receive a bonus at this time";
     alert(bonus1);
+    
 
 function calcBonus(production, products){
     var bonusAmount = parseFloat(products - production) * .50
